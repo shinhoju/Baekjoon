9217f78c91b577aebc1e4b262cfcfec556c59217
Hoju <128611443+shinhoju@users.noreply.github.com>
2025-01-18 16:06:36 +0900

    [Bronze II] Title: 시험 감독, Time: 324 ms, Memory: 232748 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.py
+++ 백준/Bronze/13458. 시험 감독/시험 감독.py
@@ -1,16 +1,26 @@
-test_places = int(input())
-test_members = list(map(int, input().split()))
+# 시험 감독
+# 모든 응시자를 감시하기 위한 감독관의 총 수 구하기
+# 단순 구현
+
+N = int(input())
+A = list(map(int, input().split()))
 B, C = map(int, input().split())
+
 result = 0
 
-for i in test_members:
-    i -= B
+for a in A:
+    # [1] 총감독관 1명
     result += 1
+    a = a - B
 
-    if i > 0:
-        result += (i//C)
+    # 총감독관 배치 후에도 더 감독해야 할 응시자 있는 경우:
+    if a > 0:
+        quo = a // C
+        re = a % C
 
-        if i % C > 0:
-            result += 1
+        if re == 0:
+            result += quo
+        else:
+            result += (quo+1)
 
 print(result)
