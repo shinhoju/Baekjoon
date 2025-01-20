8f6fce8305989f2db9aab539aa1124ef7e22a14c
Hoju <128611443+shinhoju@users.noreply.github.com>
2025-01-20 17:33:35 +0900

    [Silver I] Title: 스타트와 링크, Time: 584 ms, Memory: 165340 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,41 +1,37 @@
-from itertools import combinations, permutations
+# 스타트와 링크
+from itertools import combinations
 
 N = int(input())
-board = []
+table = []
 for _ in range(N):
-  board.append(list(map(int, input().split())))
+    table.append(list(map(int, input().split())))
 
+# [1] 팀 나누기
+teams = set()
+temp = set(range(N))
+for comb in combinations(range(N), N//2):
+    team = sorted([comb, tuple(temp-set(comb))])
+    teams.add(tuple(team))
 
-def factorial(x, e):
-  result = 1
-  while True:
-    if x != (e - 1):
-      result *= x
-      x = x - 1
-    else:
-      return result
+# [2] 능력치 table 조회하여 차 구하기
+result = 2**16
+for start, link in teams:
+    # start 팀의 능력치 합
+    start_sum = 0
+    for i in range(N//2):
+        for j in range(i, N//2):
+            s_i, s_j = start[i], start[j]
+            start_sum = start_sum + table[s_i][s_j] + table[s_j][s_i]
 
-  
-# 능력치 차이의 최소값 반환
-half_point = (factorial(N, (N // 2 + 1)) // factorial(N // 2, 1)) // 2
-units = set(range(1, N + 1))
-diff = []
+    # link 팀의 능력치 합:
+    link_sum = 0
+    for i in range(N//2):
+        for j in range(i, N//2):
+            s_i, s_j = link[i], link[j]
+            link_sum = link_sum + table[s_i][s_j] + table[s_j][s_i]
 
-for i, c in enumerate(combinations(range(1, N+1), N // 2)):
-  if i >= half_point:
-    break
-  team1 = set(c)
-  team2 = units - team1
+    temp_diff = abs(start_sum - link_sum)
+    if temp_diff < result:
+        result = temp_diff
 
-  h1 = 0
-  h2 = 0
-
-  for p1, p2 in permutations(team1, 2):
-    h1 += board[p1-1][p2-1]
-
-  for p1, p2 in permutations(team2, 2):
-    h2 += board[p1-1][p2-1]
-
-  diff.append(abs(h1 - h2))
-
-print(min(diff))
+print(result)
