53b01f4b925d91b610ef17d6b1f39bcad8832477
Hoju <128611443+shinhoju@users.noreply.github.com>
2025-01-20 18:48:32 +0900

    [Gold V] Title: 로봇 청소기, Time: 100 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,56 +1,55 @@
+# 로봇 청소기
+
 N, M = map(int, input().split())
-robot_x, robot_y, robot_d = map(int, input().split())
+x, y, d = map(int, input().split())
 
-# 0: 청소 안된 칸 / 1: 벽 / 2: 청소한 칸
-board = []
+# 0: 빈 칸, 1: 벽, -1: 청소 완료
+table = []
 for _ in range(N):
-    board.append(list(map(int, input().split())))
+    table.append(list(map(int, input().split())))
 
-# 상 우 하 좌
+# 북 동 남 서
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
 
-def in_range(x, y):
-    if x < 0 or x >= N or y < 0 or y >= M:
-        return False
-    else:
-        return True
+def empty_space(r, c):
+    flag = False        # 빈 칸이 없음
+    for n in range(4):
+        # 1칸 이라도 빈 칸이 있으면 flag 변경
+        if table[r+dx[n]][c+dy[n]] == 0:
+            flag = True
+            break
+
+    return flag
 
 
-cnt = 0
+result = 0
+
 while True:
-    # [1] 현재 칸 청소 안됐을 때, 청소
-    if board[robot_x][robot_y] == 0:
-        board[robot_x][robot_y] = 2
-        cnt += 1
+    # [1] 현재 칸이 빈 칸인 경우, 청소
+    if table[x][y] == 0:
+        table[x][y] = -1
+        result += 1
 
-    # [2-1] 현재 칸의 주변 4칸 중 청소 되지 않은 빈 칸이 없는 경우
-    flag = False        # false: 갈 수 없음 / true: 갈 수 있음
-    for i in range(4):
-        nx, ny = robot_x + dx[i], robot_y + dy[i]
-        if in_range(nx, ny):
-            if board[nx][ny] == 0:
-                flag = True
-
-    if not flag:
-        # 종료 조건: 후진 했을 때 벽인 경우
-        bd = (robot_d + 2) % 4
-        bx, by = robot_x + dx[bd], robot_y + dy[bd]
-        if board[bx][by] == 1:
+    # [2] 현재 칸의 주변 4칸 중 청소 안 된 빈 칸이 없는 경우
+    if not empty_space(x, y):
+        # [2-1] 방향 유지, 한 칸 후진, 1번으로 이동
+        # [2-2] 만약 후진한 칸이 벽이면 멈춤
+        nx, ny = x + dx[(d+2)%4], y + dy[(d+2)%4]
+        if table[nx][ny] == 1:
             break
         else:
-            robot_x, robot_y = bx, by
+            x, y = nx, ny
             continue
 
-    # [2-2] 청소 되지 않은 칸이 있는 경우
+    # [3] 현재 칸 주변 4칸 중 청소 되지 않은 칸이 있는 경우
     else:
-        # 반시계 방향 으로 90도 회전한 후, 앞에 있는 칸이 빈 칸일 경우 전진
-        robot_d = (robot_d - 1) % 4
-        rx, ry = robot_x + dx[robot_d], robot_y + dy[robot_d]
-        if in_range(rx, ry):
-            if board[rx][ry] == 0:
-                robot_x, robot_y = rx, ry
-                continue
+        # [3-1] 반시계 방향 90도 회전
+        # [3-2] 방향 기준 앞쪽 칸이 청소 안 된 칸이면 한 칸 전진
+        d = (d-1) % 4
+        nx, ny = x + dx[d], y + dy[d]
+        if table[nx][ny] == 0:
+            x, y = nx, ny
 
-print(cnt)
+print(result)
