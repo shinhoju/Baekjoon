ba476c692b352a41e8e1dd4bf0a420ae3e9a067b
Hoju <128611443+shinhoju@users.noreply.github.com>
2025-01-25 17:54:27 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 820 ms, Memory: 111612 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -1,49 +1,50 @@
 N, K = map(int, input().split())
-durability = list(map(int, input().split()))
+belt = list(map(int, input().split()))
 
-cnt = 0                 # 몇 번 단계를 반복 ?
-zeros = 0               # 내구도 0인 칸 몇 개 ?
-robot = [False] * (2 * N)
+result = 0
+zeros = 0
+robots = [False] * (2 * N)
 
 while True:
-    cnt += 1
-    belt = [0] * (2 * N)
-    # [1] 벨트가 각 칸 위에 있는 로봇과 함께 회전
-    for i in range(2 * N):
-        belt[i] = durability[i - 1]
-    durability = belt
+    result += 1
 
-    temp = [False] * (2 * N)
+    # [1] 벨트 로봇 회전
+    n_belt = [0] * (2 * N)
     for i in range(2 * N):
-        if robot[i]:
+        n_belt[i] = belt[i - 1]
+    belt = n_belt
+
+    n_robots = [False] * (2 * N)
+    for i in range(2 * N):
+        if robots[i]:
             if (i + 1) != (N - 1):
-                temp[(i + 1) % (2 * N)] = True
-    robot = temp
+                n_robots[(i + 1) % (2 * N)] = True
+    robots = n_robots
 
-    # [2] 로봇 이동
-    temp = [r for r in robot]
-    for i in range(N-2, -1, -1):
-        if robot[i]:
-            if not temp[i + 1] and durability[i + 1] >= 1:
+    # [2] 로봇 스스로 이동
+    n_robots = [r for r in robots]
+    for i in range(N-1, -1, -1):
+        if robots[i]:
+            if not n_robots[i + 1] and belt[i + 1] >= 1:
                 if (i + 1) != (N - 1):
-                    temp[i + 1] = True
-                temp[i] = False
-                durability[i + 1] -= 1
-                if durability[i + 1] == 0:
+                    n_robots[i + 1] = True
+                n_robots[i] = False
+                belt[i + 1] -= 1
+                if belt[i + 1] == 0:
                     zeros += 1
             else:
-                temp[i] = True
-    robot = temp
+                n_robots[i] = True
+    robots = n_robots
 
-    #[3] 로봇 올리기
-    if durability[0] != 0:
-        robot[0] = True
-        durability[0] -= 1
-        if durability[0] == 0:
+    # [3] 로봇 올리기
+    if belt[0] > 0:
+        robots[0] = True
+        belt[0] -= 1
+        if belt[0] == 0:
             zeros += 1
 
-    # [4] 내구도 0인 칸이 K 개 이상 --> 종료
+     # [4] 내구도 0인 칸 K개 이상이면 종료
     if zeros >= K:
         break
 
-print(cnt)
+print(result)
