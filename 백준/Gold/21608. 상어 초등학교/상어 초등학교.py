fd9eb416c9781837dfd0692ac5056e1b90696cd6
Hoju <128611443+shinhoju@users.noreply.github.com>
2025-01-26 18:08:08 +0900

    [Gold V] Title: 상어 초등학교, Time: 192 ms, Memory: 112208 KB -BaekjoonHub

--- 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
+++ 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
@@ -1,87 +1,86 @@
+# 상어 초등학교
+
 N = int(input())
-
-# 학생 번호 / 좋아 하는 친구 1,2,3,4
-students = dict()
+students = []
 for _ in range(N**2):
-    a, b, c, d, e = map(int, input().split())
-    students[a] = [b, c, d, e]
+    students.append(list(map(int, input().split())))
 
-board = [[0] * N for _ in range(N)]
+# 인접한 칸, 오 왼 상 하
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
 
-# 상 하 좌 우
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+classroom = [[0] * N for _ in range(N)]
 
-def find_friends(s, x, y):
-    global visited, board
-    max_v = 0
-    for n in range(4):
-        nx, ny = x + dx[n], y + dy[n]
-        if nx < 0 or nx >= N or ny < 0 or ny >= N:
-            continue
-        # 인접한 칸에 친한 친구가 있으면
-        if board[nx][ny] in s:
-            visited[x][y] += 1
-            max_v = max(max_v, visited[x][y])
-
-    return max_v
+student_dict = {}
 
 
-for student, friends in students.items():
-    visited = [[0] * N for _ in range(N)]
-    # [1] 비어 있는 칸 중 좋아 하는 학생이 인접한 칸에 가장 많은 칸
-    # visited 각 칸에 좋아 하는 학생 수
-    max_v = 0
+for student in students:
+    # [1] 비어있는 칸 중에서 좋아하는 학생이 인접한 칸에 가장 많은 칸 선택
+    s = student[0]
+    friends = student[1:]
+    student_dict[s] = friends
+
+    candidate_seat1 = []
+    num_like = -1
     for i in range(N):
         for j in range(N):
-            if not board[i][j]:
-                v = find_friends(friends, i, j)
-                max_v = max(max_v, v)
-            else:
-                # 다른 학생이 있는 칸일 경우, -1 표시
-                visited[i][j] = -1
+            # 누가 앉아 있으면 자리 선택 못함
+            if classroom[i][j] != 0:
+                continue
 
-    max_list = []
-    for i in range(N):
-        for j in range(N):
-            if visited[i][j] == max_v:
-                max_list.append((i, j))
-
-    if len(max_list) == 1:
-        x, y = max_list[0]
-        board[x][y] = student
-    else:
-        # [2] 1을 만족 하는 칸이 여러 개일 경우, 인접 칸 중 빈 칸이 가장 많은 칸
-        x, y = 0, 0
-        max_cnt = 0
-        for i, j in max_list[::-1]:
-            cnt = 0
+            like = 0
             for n in range(4):
                 ni, nj = i + dx[n], j + dy[n]
                 if ni < 0 or ni >= N or nj < 0 or nj >= N:
                     continue
-                if not board[ni][nj]:
-                    cnt += 1
-            if max_cnt <= cnt:
-                max_cnt = cnt
-                x, y = i, j
-        board[x][y] = student
+                if classroom[ni][nj] in friends:
+                    like += 1
+
+            if num_like < like:
+                num_like = like
+                candidate_seat1 = [(i, j)]
+            elif num_like == like:
+                candidate_seat1.append((i, j))
+
+
+    # 후보 자리가 1개이면 그 자리에 앉힘
+    if len(candidate_seat1) == 1:
+        classroom[candidate_seat1[0][0]][candidate_seat1[0][1]] = s
+
+    # [2] 1 만족 자리가 여러 개인 경우: 가장 빈 칸이 많은 자리 선택
+    else:
+        candidate_seat2 = []
+        num_vacancy = -1
+        for cx, cy in candidate_seat1:
+            vacancy = 0
+            for n in range(4):
+                ni, nj = cx + dx[n], cy + dy[n]
+                if ni < 0 or ni >= N or nj < 0 or nj >= N:
+                    continue
+                if classroom[ni][nj] == 0:
+                    vacancy += 1
+
+            if num_vacancy < vacancy:
+                num_vacancy = vacancy
+                candidate_seat2 = [(cx, cy)]
+            elif num_vacancy == vacancy:
+                candidate_seat2.append((cx, cy))
+
+        # [3] 2 만족 칸 여러 개일 경우: 행/열 번호 가장 작은 자리
+        classroom[candidate_seat2[0][0]][candidate_seat2[0][1]] = s
 
 # 만족도 구하기
-sum_v = 0
+result = 0
 for i in range(N):
     for j in range(N):
-        v = 0
-        s = board[i][j]
+        temp = 0
         for n in range(4):
             ni, nj = i + dx[n], j + dy[n]
             if ni < 0 or ni >= N or nj < 0 or nj >= N:
                 continue
-            if board[ni][nj] in students[s]:
-                v += 1
-        if v == 0:
-            sum_v += 0
-        else:
-            sum_v += (10 ** (v-1))
+            if classroom[ni][nj] in student_dict[classroom[i][j]]:
+                temp += 1
+        if temp > 0:
+            result += (10 ** (temp - 1))
 
-print(sum_v)
+print(result)
