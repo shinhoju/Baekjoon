f57124e1d255a27bf6e76a400c3e910f98a2eeb5
Hoju <128611443+shinhoju@users.noreply.github.com>
2025-03-27 21:20:23 +0900

    [Gold IV] Title: 마법사 상어와 파이어볼, Time: 688 ms, Memory: 196860 KB -BaekjoonHub

--- 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.py
+++ 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.py
@@ -1,67 +1,59 @@
+# 20056. 마법사 상어와 파이어 볼
+
 from copy import deepcopy
 
 N, M, K = map(int, input().split())
-fire = {}
-# x, y, m, s, d (위치 / 무게 / 거리 / 방향)
+fire = dict()
 for _ in range(M):
-    x, y, m, s, d = map(int, input().split())
-    fire[(x, y)] = [(m, s, d)]
+    r, c, m, s, d = map(int, input().split())
+    fire[(r, c)] = [[m, s, d]]
 
 dx = [-1, -1, 0, 1, 1, 1, 0, -1]
 dy = [0, 1, 1, 1, 0, -1, -1, -1]
 
 for _ in range(K):
-    # [1] 파이어 볼 이동
-    nfire = {}
-    next_coord = {}
-    for (x, y), item in fire.items():
-        for m, s, d in item:
-            nx = (x + dx[d] * s + N) % N
-            ny = (y + dy[d] * s + N) % N
+    # [1] 모든 파이어 볼이 d 방향, s칸 만큼 이동
+    new_fire = {}
+    for ri, ci in fire.keys():
+        for mi, si, di in fire[(ri, ci)]:
+            nr = (ri + dx[di] * si) % N
+            nc = (ci + dy[di] * si) % N
 
-            if (nx, ny) in nfire.keys():
-                nfire[(nx, ny)].append((m, s, d))
+            if (nr, nc) in new_fire.keys():
+                new_fire[(nr, nc)].append([mi, si, di])
             else:
-                nfire[(nx, ny)] = [(m, s, d)]
+                new_fire[(nr, nc)] = [[mi, si, di]]
+    fire = new_fire
 
-    temp = deepcopy(nfire)
+    # [2] 이동이 끝난 후, 2개 이상의 파이어 볼이 있는 칸
+    new_fire = deepcopy(fire)
+    for ri, ci in fire.keys():
+        if len(fire[(ri, ci)]) >= 2:
+            sum_m = 0
+            sum_s = 0
+            sum_d = []
+            for mi, si, di in fire[(ri, ci)]:
+                sum_m += mi
+                sum_s += si
+                sum_d.append(0 if di % 2 == 0 else 1)
 
-    for (x, y), item in nfire.items():
-        if len(item) >= 2:
-            sm = 0
-            ss = 0
-            sd = []
+            new_m = sum_m // 5
+            new_s = sum_s // len(fire[(ri, ci)])
 
-            for m, s, d in item:
-                sm += m
-                ss += s
-                sd.append(d)
-
-            dm = sm // 5
-            ds = ss // len(item)
-
-            if all(ddd % 2 == 0 for ddd in sd):
-                dd = [0, 2, 4, 6]
-            elif all(ddd % 2 == 1 for ddd in sd):
-                dd = [0, 2, 4, 6]
+            if new_m == 0:
+                new_fire.pop((ri, ci))
             else:
-                dd = [1, 3, 5, 7]
-
-            tmp = []
-
-            if not dm:
-                temp.pop((x, y))
-            else:
-                for ddd in dd:
-                    tmp.append((dm, ds, ddd))
-                temp[(x, y)] = tmp
-
-    fire = temp
+                if all(num % 2 == 0 for num in sum_d) or all(num % 2 == 1 for num in sum_d):
+                    new_fire[(ri, ci)] = [[new_m, new_s, 0], [new_m, new_s, 2], [new_m, new_s, 4], [new_m, new_s, 6]]
+                else:
+                    new_fire[(ri, ci)] = [[new_m, new_s, 1], [new_m, new_s, 3], [new_m, new_s, 5], [new_m, new_s, 7]]
+    fire = new_fire
 
 
+# [3] 남은 질량
 result = 0
-for lst in fire.values():
-    for m, _, _ in lst:
+for f in fire.values():
+    for m, _, _ in f:
         result += m
 
 print(result)
