e24d6bc62ec34df2738effb87da15b213f81c778
Hoju <128611443+shinhoju@users.noreply.github.com>
2025-08-24 01:36:05 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 568 ms, Memory: 112752 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
@@ -1,67 +1,52 @@
-# 마법사 상어와 비바라기
-# 구현, 시뮬레이션
+# 21610. 마법사 상어와 비바라기
 
 N, M = map(int, input().split())
-
-A = []
+arr = []
 for _ in range(N):
-    A.append(list(map(int, input().split())))
-
+    arr.append(list(map(int, input().split())))
 move = []
 for _ in range(M):
-    move.append(list(map(int, input().split())))
+    t1, t2 = map(int, input().split())
+    move.append((t1-1, t2))
+cloud = [(N-1, 0), (N-1, 1), (N-2, 0), (N-2, 1)]
 
-# 1번 부터 ←, ↖, ↑, ↗, →, ↘, ↓, ↙
-dx = [0, 0, -1, -1, -1, 0, 1, 1, 1]
-dy = [0, -1, -1, 0, 1, 1, 1, 0, -1]
-
-# 구름 위치 저장
-cloud = [(N-2, 0), (N-2, 1), (N-1, 0), (N-1, 1)]
+dx = [0, -1, -1, -1, 0, 1, 1, 1]
+dy = [-1, -1, 0, 1, 1, 1, 0, -1]
 
 for d, s in move:
     # [1] 모든 구름 이동
-    new_cloud = []
+    # [2] 구름이 있는 위치 -> 바구니 + 1
+    next_cloud = []
     for cx, cy in cloud:
-        # 행, 열 연결 되어 있음
-        ncx, ncy = cx + s * dx[d], cy + s * dy[d]
-        if ncx < 0:
-            ncx = N - (abs(ncx) % N)
-        if ncx >= N:
-            ncx = ncx % N
-        if ncy < 0:
-            ncy = N - (abs(ncy) % N)
-        if ncy >= N:
-            ncy = ncy % N
-        new_cloud.append((ncx, ncy))
-        # [2] 비가 내려 구름이 있는 칸 바구니 저장된 물의 양 + 1
-        A[ncx][ncy] += 1
+        ncx, ncy = (cx + s * dx[d]) % N, (cy + s * dy[d]) % N
+        next_cloud.append((ncx, ncy))
+        arr[ncx][ncy] += 1
 
-    # [4] 물 복사 버그: new_cloud 칸에서 대각선 검사, 연결된 칸 x
-    for cx, cy in new_cloud:
-        bug = 0
-        for n in [2, 4, 6, 8]:
-            ncx, ncy = cx + dx[n], cy + dy[n]
+    # [3] 물이 증가한 칸에 마법 -> 대각선 방향 거리 1인 칸에 물 존재 바구니 수 만큼 물 증가
+    # 물이 증가한 칸 = 새로운 구름 위치
+    for cx, cy in next_cloud:
+        basket = 0
+        for dr in range(1, 8, 2):
+            ncx, ncy = cx + dx[dr], cy + dy[dr]
             if ncx < 0 or ncx >= N or ncy < 0 or ncy >= N:
                 continue
-            if A[ncx][ncy] > 0:
-                bug += 1
-        A[cx][cy] += bug
+            if arr[ncx][ncy] > 0:
+                basket += 1
+        arr[cx][cy] += basket
 
-    # [5] 바구니 물 2 이상인 칸에 구름이 생기고 물의 양 -2, cloud 저장된 칸 x
-    next_cloud = []
+    # [4] 물의 양 2 이상인 칸에 구름 생김 -> 물의 양 - 2
+    # 3에서 사라진 구름 칸이 아니어야 함
+    cloud = []
     for i in range(N):
         for j in range(N):
-            if (i, j) in new_cloud:
-                continue
-            if A[i][j] >= 2:
-                next_cloud.append((i, j))
-                A[i][j] -= 2
+            if arr[i][j] >= 2 and not (i, j) in next_cloud:
+                cloud.append((i, j))
+                arr[i][j] -= 2
 
-    cloud = next_cloud
-
+# 바구니에 들어있는 물의 합
 result = 0
 for i in range(N):
     for j in range(N):
-        result += A[i][j]
+        result += arr[i][j]
 
 print(result)
