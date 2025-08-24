a829a36daff8f138daacdd281254196332e8bd69
Hoju <128611443+shinhoju@users.noreply.github.com>
2025-08-24 18:38:51 +0900

    [Gold IV] Title: 뱀, Time: 124 ms, Memory: 114200 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,57 +1,61 @@
+# 3190. 뱀
+
 from collections import deque
 
 N = int(input())
-
 K = int(input())
+
 apples = []
 for _ in range(K):
     apples.append(tuple(map(int, input().split())))
 
 L = int(input())
-move = dict()
+move = {}                       # -1: 왼쪽 / 1: 오른쪽 회전
 for _ in range(L):
     x, c = input().split()
-    move[int(x)] = c
+    move[int(x)] = -1 if c == "L" else 1
 
-# 오른 아래 왼 위 (오른쪽 회전)
+snake = deque()
+snake.append((1, 1))
+snake_d = 0
+
+# 오른쪽 -> 시계 방향
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
-# snake: 몸 위치 좌표, snake_d: 머리 방향
-snake = deque()
-snake_d = 0
-snake.append((1, 1))
+
+def in_range(x, y):
+    if x < 1 or x > N or y < 1 or y > N:
+        return False
+    else:
+        return True
+
 
 result = 0
-
-
 while True:
-    if result in move.keys():
-        if move[result] == 'L':
-            snake_d = (snake_d + 3) % 4
-        if move[result] == 'D':
-            snake_d = (snake_d + 1) % 4
+    result += 1
 
     # [1] 머리를 다음 칸에 위치
-    head_x = snake[0][0] + dx[snake_d]
-    head_y = snake[0][1] + dy[snake_d]
+    hx, hy = snake[0][0], snake[0][1]
+    n_hx, n_hy = hx + dx[snake_d], hy + dy[snake_d]
 
-    # [2] 벽이나 자기 몸과 부딛히면 게임 끝
-    if head_x < 1 or head_x > N or head_y < 1 or head_y > N:
-        result += 1
+    # 종료 조건: 벽이나 자기 자신의 몸과 부딛힘
+    if not in_range(n_hx, n_hy):
         break
-    if (head_x, head_y) in snake:
-        result += 1
+    if (n_hx, n_hy) in snake:
         break
 
-    # [3-1] 이동한 칸에 사과가 있으면, 그 칸에 있던 사과 x, 꼬리 안움직
-    if (head_x, head_y) in apples:
-        apples.remove((head_x, head_y))
-        snake.appendleft((head_x, head_y))
-    # [3-2] 이동한 칸에 사과 없으면, 몸 길이를 줄여 꼬리가 위치한 칸 비움
+    # [2] 사과 o: 사과 먹고 꼬리 움직 x
+    if (n_hx, n_hy) in apples:
+        snake.appendleft((n_hx, n_hy))
+        apples.remove((n_hx, n_hy))
+    # [3] 사과 x: 꼬리 - 1칸
     else:
-        snake.appendleft((head_x, head_y))
         snake.pop()
-    result += 1
+        snake.appendleft((n_hx, n_hy))
+
+    # [4] 방향 전환
+    if result in move.keys():
+        snake_d = (snake_d + move[result]) % 4
 
 print(result)
