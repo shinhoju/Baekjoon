ce13f6e12e042047ebf9fdfbf69ea87bbf232747
Hoju <128611443+shinhoju@users.noreply.github.com>
2025-08-26 10:57:00 +0900

    [Gold IV] Title: 연구소, Time: 448 ms, Memory: 119796 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,57 +1,82 @@
+# 14502. 연구소
+
 from collections import deque
-from itertools import combinations
 
 N, M = map(int, input().split())
-maps = []
+arr = []
 for _ in range(N):
-    maps.append(list(map(int, input().split())))
+    arr.append(list(map(int, input().split())))
 
-cnt = 0
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
 
-def count_virus():
-    global n_maps
-    cnt = 0
-    visited = [[False] * M for _ in range(N)]
-    queue = deque()
+# 바이러스 / 빈 칸 위치 찾기
+virus = []
+empty = []
+for i in range(N):
+    for j in range(M):
+        if arr[i][j] == 2:
+            virus.append((i, j))
+        if arr[i][j] == 0:
+            empty.append((i, j))
 
-    for i in range(N):
-        for j in range(M):
-            if n_maps[i][j] == 2:
-                queue.append((i, j))
-                visited[i][j] = True
-                cnt += 1
 
-    while queue:
-        x, y = queue.popleft()
+def comb(a, n):
+    result = []
+    if len(a) < n:
+        return result
+
+    if n == 1:
+        for i in a:
+            result.append([i])
+
+    elif n > 1:
+        for i in range(len(a) - n + 1):
+            for j in comb(a[i + 1:], n - 1):
+                result.append([a[i]] + j)
+
+    return result
+
+
+def bfs(x, y):
+    global v, n_arr
+    q = deque()
+    q.append((x, y))
+    v[x][y] = True
+
+    while q:
+        x, y = q.popleft()
         for n in range(4):
             nx, ny = x + dx[n], y + dy[n]
             if nx < 0 or nx >= N or ny < 0 or ny >= M:
                 continue
-            if not visited[nx][ny] and n_maps[nx][ny] == 0:
-                n_maps[nx][ny] = 2
-                visited[nx][ny] = True
-                queue.append((nx, ny))
-                cnt += 1
+            # 방문 x / 빈 칸이면 바이러스 퍼짐
+            if not v[nx][ny] and n_arr[nx][ny] == 0:
+                v[nx][ny] = True
+                n_arr[nx][ny] = 2
+                q.append((nx, ny))
 
-    return cnt
 
-# 3개의 연구소 세우기
-walls = 0
-empty = []
-for i in range(N):
-    for j in range(M):
-        if maps[i][j] == 0:
-            empty.append((i, j))
-        if maps[i][j] == 1:
-            walls += 1
+# 벽을 세울 세 칸 고르기
+safe_area = 0
+for w1, w2, w3 in comb(empty, 3):
+    n_arr = [a[:] for a in arr]
+    for i, j in (w1, w2, w3):
+        n_arr[i][j] = 1
 
-for es in combinations(empty, 3):
-    n_maps = [m[:] for m in maps]
-    for ex, ey in es:
-        n_maps[ex][ey] = 1
-    vir = count_virus()
-    cnt = max((N*M-vir-walls-3), cnt)
+    # 바이러스 퍼트리기
+    v = [[False] * M for _ in range(N)]
+    for vx, vy in virus:
+        bfs(vx, vy)
 
-print(cnt)
+    # 안전 지대 계산
+    tmp_safe = 0
+    for i in range(N):
+        for j in range(M):
+            if n_arr[i][j] == 0:
+                tmp_safe += 1
+
+    if tmp_safe > safe_area:
+        safe_area = tmp_safe
+
+print(safe_area)
