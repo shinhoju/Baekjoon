23a1369346d19ee26b5f4dc6ea10fe838091484a
Hoju <128611443+shinhoju@users.noreply.github.com>
2025-08-26 11:23:30 +0900

    [Gold IV] Title: 인구 이동, Time: 1132 ms, Memory: 129316 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,73 +1,62 @@
 # 16234. 인구이동
 
 from collections import deque
-from copy import deepcopy
 
 N, L, R = map(int, input().split())
-country = []
+arr = []
 for _ in range(N):
-    country.append(list(map(int, input().split())))
+    arr.append(list(map(int, input().split())))
 
-# 인구 이동이 며칠 동안 발생하는지 카운팅
-cnt = 0
-
-# 상 하 좌 우
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
 
 
+# unit = bfs(i, j)
 def bfs(x, y):
-    global dx, dy, visited
+    global v, arr
+    u = [(x, y)]
+    u_sum = arr[x][y]
+    q = deque()
+    q.append((x, y))
+    v[x][y] = True
 
-    visited[x][y] = True
-    queue = deque()
-    queue.append((x, y))
-    temp_pairs = [(x, y)]
-    temp_sum = country[x][y]
-
-    while queue:
-        x, y = queue.popleft()
+    while q:
+        x, y = q.popleft()
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
             if nx < 0 or nx >= N or ny < 0 or ny >= N:
                 continue
-            if visited[nx][ny]:
-                continue
-            if L <= abs(country[x][y] - country[nx][ny]) <= R:
-                temp_pairs.append((nx, ny))
-                queue.append((nx, ny))
-                visited[nx][ny] = True
-                temp_sum += country[nx][ny]
-
-    return temp_pairs, temp_sum
+            # 조건: L <= 인구 차이 <= R
+            if not v[nx][ny] and L <= abs(arr[x][y] - arr[nx][ny]) <= R:
+                u.append((nx, ny))
+                u_sum += arr[nx][ny]
+                q.append((nx, ny))
+                v[nx][ny] = True
+    return u, u_sum
 
 
+result = 0
 while True:
     # [1] 연합 구하기
-    pairs = set()
-    pair_sum = dict()
-
-    visited = [[False] * N for _ in range(N)]
-
+    v = [[False] * N for _ in range(N)]
+    units = []
     for i in range(N):
         for j in range(N):
-            if not visited[i][j]:
-                temp, summ = bfs(i, j)
-                if len(temp) > 1:
-                    pair_sum[tuple(sorted(temp))] = summ
-                    pairs.add(tuple(sorted(temp)))
+            if not v[i][j]:
+                unit, unit_sum = bfs(i, j)
+                if len(unit) > 1:
+                    units.append((unit, unit_sum))
 
-    if len(pairs) < 1:
+    # [2] 연합 없으면 인구 이동 X => 종료 조건
+    if not len(units):
         break
-    else:
-        cnt += 1
 
-    # [2] 인구 이동: 인구수 = (연합의 인구수) / (연합을 이루고 있는 칸의 개수)
-    new_country = deepcopy(country)
-    for p in pairs:
-        new_population = pair_sum[p] // len(p)
-        for px, py in p:
-            new_country[px][py] = new_population
-    country = new_country
+    # [3] 인구 이동
+    for unit, unit_sum in units:
+        tmp_pop = unit_sum // len(unit)
+        for ux, uy in unit:
+            arr[ux][uy] = tmp_pop
 
-print(cnt)
+    result += 1
+
+print(result)
