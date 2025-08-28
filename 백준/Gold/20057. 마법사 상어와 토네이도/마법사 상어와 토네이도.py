03a459359c312399dd04c1fd7ea8d96ed7e9287e
Hoju <128611443+shinhoju@users.noreply.github.com>
2025-08-28 16:30:50 +0900

    [Gold III] Title: 마법사 상어와 토네이도, Time: 276 ms, Memory: 113616 KB -BaekjoonHub

--- 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.py
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.py
@@ -1,353 +1,56 @@
 # 20057. 마법사 상어와 토네이도
 
 N = int(input())
-sand = []
+arr = []
 for _ in range(N):
-    sand.append(list(map(int, input().split())))
+    arr.append(list(map(int, input().split())))
+tx, ty, td, tl = N // 2, N // 2, 0, 1          # 토네이도 위치 좌표, 방향
 
-# 왼 아 오 위
 dx = [0, 1, 0, -1]
 dy = [-1, 0, 1, 0]
 
-# 토네이도 위치 좌표, 방향
-x, y = (N-1) // 2, (N-1) // 2
-d = 0
+mul = [2, 10, 7, 1, 5, 10, 7, 1, 2, 0]
+ax = [[-2, -1, -1, -1, 0, 1, 1, 1, 2, 0],
+      [0, 1, 0, -1, 2, 1, 0, -1, 0, 1],
+      [2, 1, 1, 1, 0, -1, -1, -1, -2, 0],
+      [0, -1, 0, 1, -2, -1, 0, 1, 0, -1]]
 
-schedule = []
-for i in range(1, N+1):
-    schedule.append(i)
-    schedule.append(i)
-
-count_s = 0
-temp_s = 0
-
-# 밖으로 나간 모래 수
-result = 0
+ay = [[0, -1, 0, 1, -2, -1, 0, 1, 0, -1],
+      [-2, -1, -1, -1, 0, 1, 1, 1, 2, 0],
+      [0, 1, 0, -1, 2, 1, 0, -1, 0, 1],
+      [2, 1, 1, 1, 0, -1, -1, -1, -2, 0]]
 
 
-def in_range(r, c):
-    if r < 0 or r >= N or c < 0 or c >= N:
-        return False
-    else:
-        return True
+cur_l = answer = 0               # 현재 변의 길이
+flag = False            # 변의 길이 증가 할 말
+while (tx, ty) != (0, 0):
+    tx, ty = tx + dx[td], ty + dy[td]
 
+    if arr[tx][ty] > 0:
+        # 모래 날리기
+        tsand = arr[tx][ty]
+        arr[tx][ty] = sum_s = 0     # sum_s: 날라간 모래 총 합
 
-def tornado(r, c, direction):
-    global sand, nx, ny
-    out_sand = 0
-    spread_sand = 0
+        for i in range(10):
+            nx, ny = tx + ax[td][i], ty + ay[td][i]
+            s = (tsand * mul[i]) // 100
+            if i == 9:
+                s = tsand - sum_s
 
-    if direction == 0:
-        # 왼쪽 날리기
-        if in_range(x - 1, y):
-            sand[x - 1][y] += int(sand[nx][ny] * 0.01)
-            spread_sand += int(sand[nx][ny] * 0.01)
-        else:
-            out_sand += int(sand[nx][ny] * 0.01)
-            spread_sand += int(sand[nx][ny] * 0.01)
+            if nx < 0 or nx >= N or ny < 0 or ny >= N:
+                answer += s
+            else:
+                arr[nx][ny] += s
+            sum_s += s
 
-        if in_range(x + 1, y):
-            sand[x + 1][y] += int(sand[nx][ny] * 0.01)
-            spread_sand += int(sand[nx][ny] * 0.01)
-        else:
-            out_sand += int(sand[nx][ny] * 0.01)
-            spread_sand += int(sand[nx][ny] * 0.01)
+    cur_l += 1
+    if cur_l == tl:
+        td = (td + 1) % 4
+        cur_l = 0
+        if not flag:            # 길이 증가 x
+            flag = True
+        else:                   # 길이 증가 o
+            flag = False
+            tl += 1
 
-        if in_range(x - 1, y - 1):
-            sand[x - 1][y - 1] += int(sand[nx][ny] * 0.07)
-            spread_sand += int(sand[nx][ny] * 0.07)
-        else:
-            out_sand += int(sand[nx][ny] * 0.07)
-            spread_sand += int(sand[nx][ny] * 0.07)
-
-        if in_range(x + 1, y - 1):
-            sand[x + 1][y - 1] += int(sand[nx][ny] * 0.07)
-            spread_sand += int(sand[nx][ny] * 0.07)
-        else:
-            out_sand += int(sand[nx][ny] * 0.07)
-            spread_sand += int(sand[nx][ny] * 0.07)
-
-        if in_range(x - 2, y - 1):
-            sand[x - 2][y - 1] += int(sand[nx][ny] * 0.02)
-            spread_sand += int(sand[nx][ny] * 0.02)
-        else:
-            out_sand += int(sand[nx][ny] * 0.02)
-            spread_sand += int(sand[nx][ny] * 0.02)
-
-        if in_range(x + 2, y - 1):
-            sand[x + 2][y - 1] += int(sand[nx][ny] * 0.02)
-            spread_sand += int(sand[nx][ny] * 0.02)
-        else:
-            out_sand += int(sand[nx][ny] * 0.02)
-            spread_sand += int(sand[nx][ny] * 0.02)
-
-        if in_range(x - 1, y - 2):
-            sand[x - 1][y - 2] += int(sand[nx][ny] * 0.1)
-            spread_sand += int(sand[nx][ny] * 0.1)
-        else:
-            out_sand += int(sand[nx][ny] * 0.1)
-            spread_sand += int(sand[nx][ny] * 0.1)
-
-        if in_range(x + 1, y - 2):
-            sand[x + 1][y - 2] += int(sand[nx][ny] * 0.1)
-            spread_sand += int(sand[nx][ny] * 0.1)
-        else:
-            out_sand += int(sand[nx][ny] * 0.1)
-            spread_sand += int(sand[nx][ny] * 0.1)
-
-        if in_range(x, y - 3):
-            sand[x][y - 3] += int(sand[nx][ny] * 0.05)
-            spread_sand += int(sand[nx][ny] * 0.05)
-        else:
-            out_sand += int(sand[nx][ny] * 0.05)
-            spread_sand += int(sand[nx][ny] * 0.05)
-
-        if in_range(x, y - 2):
-            sand[x][y - 2] += (sand[nx][ny] - spread_sand)
-        else:
-            out_sand += (sand[nx][ny] - spread_sand)
-
-        return out_sand
-
-
-    if direction == 1:
-        # 왼쪽 날리기
-        if in_range(x, y-1):
-            sand[x][y-1] += int(sand[nx][ny] * 0.01)
-            spread_sand += int(sand[nx][ny] * 0.01)
-        else:
-            out_sand += int(sand[nx][ny] * 0.01)
-            spread_sand += int(sand[nx][ny] * 0.01)
-
-        if in_range(x, y+1):
-            sand[x][y+1] += int(sand[nx][ny] * 0.01)
-            spread_sand += int(sand[nx][ny] * 0.01)
-        else:
-            out_sand += int(sand[nx][ny] * 0.01)
-            spread_sand += int(sand[nx][ny] * 0.01)
-
-        if in_range(x+1, y-1):
-            sand[x + 1][y - 1] += int(sand[nx][ny] * 0.07)
-            spread_sand += int(sand[nx][ny] * 0.07)
-        else:
-            out_sand += int(sand[nx][ny] * 0.07)
-            spread_sand += int(sand[nx][ny] * 0.07)
-
-        if in_range(x+1, y+1):
-            sand[x + 1][y + 1] += int(sand[nx][ny] * 0.07)
-            spread_sand += int(sand[nx][ny] * 0.07)
-        else:
-            out_sand += int(sand[nx][ny] * 0.07)
-            spread_sand += int(sand[nx][ny] * 0.07)
-
-        if in_range(x+1, y-2):
-            sand[x + 1][y - 2] += int(sand[nx][ny] * 0.02)
-            spread_sand += int(sand[nx][ny] * 0.02)
-        else:
-            out_sand += int(sand[nx][ny] * 0.02)
-            spread_sand += int(sand[nx][ny] * 0.02)
-
-        if in_range(x+1, y +2):
-            sand[x + 1][y +2] += int(sand[nx][ny] * 0.02)
-            spread_sand += int(sand[nx][ny] * 0.02)
-        else:
-            out_sand += int(sand[nx][ny] * 0.02)
-            spread_sand += int(sand[nx][ny] * 0.02)
-
-        if in_range(x +2, y - 1):
-            sand[x +2][y - 1] += int(sand[nx][ny] * 0.1)
-            spread_sand += int(sand[nx][ny] * 0.1)
-        else:
-            out_sand += int(sand[nx][ny] * 0.1)
-            spread_sand += int(sand[nx][ny] * 0.1)
-
-        if in_range(x + 2, y +1):
-            sand[x + 2][y+1] += int(sand[nx][ny] * 0.1)
-            spread_sand += int(sand[nx][ny] * 0.1)
-        else:
-            out_sand += int(sand[nx][ny] * 0.1)
-            spread_sand += int(sand[nx][ny] * 0.1)
-
-        if in_range(x+3, y):
-            sand[x+3][y] += int(sand[nx][ny] * 0.05)
-            spread_sand += int(sand[nx][ny] * 0.05)
-        else:
-            out_sand += int(sand[nx][ny] * 0.05)
-            spread_sand += int(sand[nx][ny] * 0.05)
-
-        if in_range(x+2, y):
-            sand[x+2][y] += (sand[nx][ny] - spread_sand)
-        else:
-            out_sand += (sand[nx][ny] - spread_sand)
-
-        return out_sand
-
-
-    if direction == 2:
-        # 왼쪽 날리기
-        if in_range(x - 1, y):
-            sand[x - 1][y] += int(sand[nx][ny] * 0.01)
-            spread_sand += int(sand[nx][ny] * 0.01)
-        else:
-            out_sand += int(sand[nx][ny] * 0.01)
-            spread_sand += int(sand[nx][ny] * 0.01)
-
-        if in_range(x + 1, y):
-            sand[x + 1][y] += int(sand[nx][ny] * 0.01)
-            spread_sand += int(sand[nx][ny] * 0.01)
-        else:
-            out_sand += int(sand[nx][ny] * 0.01)
-            spread_sand += int(sand[nx][ny] * 0.01)
-
-        if in_range(x - 1, y + 1):
-            sand[x - 1][y + 1] += int(sand[nx][ny] * 0.07)
-            spread_sand += int(sand[nx][ny] * 0.07)
-        else:
-            out_sand += int(sand[nx][ny] * 0.07)
-            spread_sand += int(sand[nx][ny] * 0.07)
-
-        if in_range(x + 1, y + 1):
-            sand[x + 1][y + 1] += int(sand[nx][ny] * 0.07)
-            spread_sand += int(sand[nx][ny] * 0.07)
-        else:
-            out_sand += int(sand[nx][ny] * 0.07)
-            spread_sand += int(sand[nx][ny] * 0.07)
-
-        if in_range(x - 2, y + 1):
-            sand[x - 2][y + 1] += int(sand[nx][ny] * 0.02)
-            spread_sand += int(sand[nx][ny] * 0.02)
-        else:
-            out_sand += int(sand[nx][ny] * 0.02)
-            spread_sand += int(sand[nx][ny] * 0.02)
-
-        if in_range(x + 2, y + 1):
-            sand[x + 2][y + 1] += int(sand[nx][ny] * 0.02)
-            spread_sand += int(sand[nx][ny] * 0.02)
-        else:
-            out_sand += int(sand[nx][ny] * 0.02)
-            spread_sand += int(sand[nx][ny] * 0.02)
-
-        if in_range(x - 1, y + 2):
-            sand[x - 1][y + 2] += int(sand[nx][ny] * 0.1)
-            spread_sand += int(sand[nx][ny] * 0.1)
-        else:
-            out_sand += int(sand[nx][ny] * 0.1)
-            spread_sand += int(sand[nx][ny] * 0.1)
-
-        if in_range(x + 1, y + 2):
-            sand[x + 1][y + 2] += int(sand[nx][ny] * 0.1)
-            spread_sand += int(sand[nx][ny] * 0.1)
-        else:
-            out_sand += int(sand[nx][ny] * 0.1)
-            spread_sand += int(sand[nx][ny] * 0.1)
-
-        if in_range(x, y + 3):
-            sand[x][y + 3] += int(sand[nx][ny] * 0.05)
-            spread_sand += int(sand[nx][ny] * 0.05)
-        else:
-            out_sand += int(sand[nx][ny] * 0.05)
-            spread_sand += int(sand[nx][ny] * 0.05)
-
-        if in_range(x, y + 2):
-            sand[x][y + 2] += (sand[nx][ny] - spread_sand)
-        else:
-            out_sand += (sand[nx][ny] - spread_sand)
-
-        return out_sand
-
-
-    if direction == 3:
-        # 왼쪽 날리기
-        if in_range(x, y-1):
-            sand[x][y-1] += int(sand[nx][ny] * 0.01)
-            spread_sand += int(sand[nx][ny] * 0.01)
-        else:
-            out_sand += int(sand[nx][ny] * 0.01)
-            spread_sand += int(sand[nx][ny] * 0.01)
-
-        if in_range(x, y+1):
-            sand[x][y+1] += int(sand[nx][ny] * 0.01)
-            spread_sand += int(sand[nx][ny] * 0.01)
-        else:
-            out_sand += int(sand[nx][ny] * 0.01)
-            spread_sand += int(sand[nx][ny] * 0.01)
-
-        if in_range(x-1, y-1):
-            sand[x - 1][y - 1] += int(sand[nx][ny] * 0.07)
-            spread_sand += int(sand[nx][ny] * 0.07)
-        else:
-            out_sand += int(sand[nx][ny] * 0.07)
-            spread_sand += int(sand[nx][ny] * 0.07)
-
-        if in_range(x-1, y+1):
-            sand[x - 1][y + 1] += int(sand[nx][ny] * 0.07)
-            spread_sand += int(sand[nx][ny] * 0.07)
-        else:
-            out_sand += int(sand[nx][ny] * 0.07)
-            spread_sand += int(sand[nx][ny] * 0.07)
-
-        if in_range(x-1, y-2):
-            sand[x - 1][y - 2] += int(sand[nx][ny] * 0.02)
-            spread_sand += int(sand[nx][ny] * 0.02)
-        else:
-            out_sand += int(sand[nx][ny] * 0.02)
-            spread_sand += int(sand[nx][ny] * 0.02)
-
-        if in_range(x-1, y +2):
-            sand[x -1][y +2] += int(sand[nx][ny] * 0.02)
-            spread_sand += int(sand[nx][ny] * 0.02)
-        else:
-            out_sand += int(sand[nx][ny] * 0.02)
-            spread_sand += int(sand[nx][ny] * 0.02)
-
-        if in_range(x -2, y - 1):
-            sand[x -2][y - 1] += int(sand[nx][ny] * 0.1)
-            spread_sand += int(sand[nx][ny] * 0.1)
-        else:
-            out_sand += int(sand[nx][ny] * 0.1)
-            spread_sand += int(sand[nx][ny] * 0.1)
-
-        if in_range(x - 2, y +1):
-            sand[x - 2][y+1] += int(sand[nx][ny] * 0.1)
-            spread_sand += int(sand[nx][ny] * 0.1)
-        else:
-            out_sand += int(sand[nx][ny] * 0.1)
-            spread_sand += int(sand[nx][ny] * 0.1)
-
-        if in_range(x-3, y):
-            sand[x-3][y] += int(sand[nx][ny] * 0.05)
-            spread_sand += int(sand[nx][ny] * 0.05)
-        else:
-            out_sand += int(sand[nx][ny] * 0.05)
-            spread_sand += int(sand[nx][ny] * 0.05)
-
-        if in_range(x-2, y):
-            sand[x-2][y] += (sand[nx][ny] - spread_sand)
-        else:
-            out_sand += (sand[nx][ny] - spread_sand)
-
-        return out_sand
-
-
-
-while True:
-    if (x, y) == (0, 0):
-        break
-
-    nx, ny = x + dx[d], y + dy[d]
-
-    # 모래 날리기
-    if sand[nx][ny] > 0:
-        temp = tornado(x, y, d)
-        result += temp
-
-    # 회전 코드
-    temp_s += 1
-    if temp_s == schedule[count_s]:
-        d = (d + 1) % 4
-        temp_s = 0
-        count_s += 1
-
-    x, y = nx, ny
-
-print(result)
+print(answer)
